f79ab3b73c7ebf4f600d4b14252e4469b921ecb8
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-03 14:05:36 +0700

    1.10.2: Added New Laser type - Infected Crystallize
    - Rewrite laser rendering

--- 1.10.2/src/main/java/stevekung/mods/moreplanets/client/model/ModelLaser.java
+++ 1.10.2/src/main/java/stevekung/mods/moreplanets/client/model/ModelLaser.java
@@ -11,10 +11,9 @@ public class ModelLaser extends ModelBase
     {
         this.textureWidth = 64;
         this.textureHeight = 32;
-
         this.laser = new ModelRenderer(this, 0, 0);
-        this.laser.addBox(0F, 0F, 0F, 1, 1, 2, 0F);
-        this.laser.setRotationPoint(0F, 0F, 0F);
+        this.laser.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.laser.addBox(-0.5F, -0.5F, -1.0F, 1, 1, 2, 0.0F);
     }
 
     public void renderLaser()
