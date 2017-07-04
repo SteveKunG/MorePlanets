a03f78dc78dd94eabbfa879732663f729b381b6c
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-05 05:21:30 +0700

    1.11.2: Minor improvement for Dark Energy Core

--- 1.11.2/src/main/java/stevekung/mods/moreplanets/integration/jei/dark_energy_transform/DarkEnergyTransformRecipeWrapper.java
+++ 1.11.2/src/main/java/stevekung/mods/moreplanets/integration/jei/dark_energy_transform/DarkEnergyTransformRecipeWrapper.java
@@ -1,11 +1,13 @@
 package stevekung.mods.moreplanets.integration.jei.dark_energy_transform;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.item.ItemStack;
 import stevekung.mods.moreplanets.recipe.DarkEnergyRecipeData;
 
@@ -25,13 +27,17 @@ public class DarkEnergyTransformRecipeWrapper extends BlankRecipeWrapper
     @Override
     public void drawInfo(Minecraft mc, int recipeWidth, int recipeHeight, int mouseX, int mouseY)
     {
-        mc.currentScreen.drawCenteredString(mc.fontRendererObj, "Time : " + this.time, 50, 38, 16777215);
+        int time = this.time / 20;
+        String text = "Time : " + time + "s * Count";
+        FontRenderer fontRendererObj = mc.fontRendererObj;
+        int width = fontRendererObj.getStringWidth(text);
+        mc.fontRendererObj.drawString(text, recipeWidth - 18 - width, 38, Color.gray.getRGB());
     }
 
     @Override
     public void getIngredients(IIngredients ingredients)
     {
-        ingredients.setInput(ItemStack.class, new ArrayList<>(this.input.values()));
+        ingredients.setInput(ItemStack.class, new ArrayList(this.input.values()));
         ingredients.setOutput(ItemStack.class, this.output);
     }
 }
