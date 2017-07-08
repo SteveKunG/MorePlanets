d4490dec82f0e529e6200e5d2762d2927b4d9cf9
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-08 10:32:14 +0700

    1.10.2: Minor rendering change for Change Log GUI

--- 1.10.2/src/main/java/stevekung/mods/moreplanets/client/gui/GuiChangeLogSlot.java
+++ 1.10.2/src/main/java/stevekung/mods/moreplanets/client/gui/GuiChangeLogSlot.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import stevekung.mods.moreplanets.core.MorePlanetsCore;
@@ -98,5 +99,83 @@ public class GuiChangeLogSlot extends GuiSlot
         buffer.pos(this.left, this.top, 0.0D).tex(this.left / size, (this.top + (int)this.amountScrolled) / size).color(color, color, color, alpha).endVertex();
         tessellator.draw();
         GlStateManager.disableBlend();
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+        if (this.visible)
+        {
+            this.mouseX = mouseX;
+            this.mouseY = mouseY;
+            this.drawBackground();
+            int i = this.getScrollBarX();
+            int j = i + 6;
+            this.bindAmountScrolled();
+            GlStateManager.disableLighting();
+            GlStateManager.disableFog();
+            Tessellator tessellator = Tessellator.getInstance();
+            VertexBuffer vertexbuffer = tessellator.getBuffer();
+            this.drawContainerBackground(tessellator);
+            int k = this.left + this.width / 2 - this.getListWidth() / 2 + 2;
+            int l = this.top + 4 - (int)this.amountScrolled;
+
+            if (this.hasListHeader)
+            {
+                this.drawListHeader(k, l, tessellator);
+            }
+
+            this.drawSelectionBox(k, l, mouseX, mouseY);
+            GlStateManager.disableDepth();
+            this.overlayBackground(0, this.top, 255, 255);
+            this.overlayBackground(this.bottom, this.height, 255, 255);
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE);
+            GlStateManager.disableAlpha();
+            GlStateManager.shadeModel(7425);
+            GlStateManager.disableTexture2D();
+            vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+            vertexbuffer.pos(this.left, this.top + 4, 0.0D).tex(0.0D, 1.0D).color(0, 0, 0, 0).endVertex();
+            vertexbuffer.pos(this.right, this.top + 4, 0.0D).tex(1.0D, 1.0D).color(0, 0, 0, 0).endVertex();
+            vertexbuffer.pos(this.right, this.top, 0.0D).tex(1.0D, 0.0D).color(0, 0, 0, 100).endVertex();
+            vertexbuffer.pos(this.left, this.top, 0.0D).tex(0.0D, 0.0D).color(0, 0, 0, 100).endVertex();
+            tessellator.draw();
+            vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+            vertexbuffer.pos(this.left, this.bottom, 0.0D).tex(0.0D, 1.0D).color(0, 0, 0, 100).endVertex();
+            vertexbuffer.pos(this.right, this.bottom, 0.0D).tex(1.0D, 1.0D).color(0, 0, 0, 100).endVertex();
+            vertexbuffer.pos(this.right, this.bottom - 4, 0.0D).tex(1.0D, 0.0D).color(0, 0, 0, 0).endVertex();
+            vertexbuffer.pos(this.left, this.bottom - 4, 0.0D).tex(0.0D, 0.0D).color(0, 0, 0, 0).endVertex();
+            tessellator.draw();
+            int j1 = this.getMaxScroll();
+
+            if (j1 > 0)
+            {
+                int k1 = (this.bottom - this.top) * (this.bottom - this.top) / this.getContentHeight();
+                k1 = MathHelper.clamp_int(k1, 32, this.bottom - this.top - 8);
+                int l1 = (int)this.amountScrolled * (this.bottom - this.top - k1) / j1 + this.top;
+
+                if (l1 < this.top)
+                {
+                    l1 = this.top;
+                }
+                vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+                vertexbuffer.pos(i, l1 + k1, 0.0D).tex(0.0D, 1.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(j, l1 + k1, 0.0D).tex(1.0D, 1.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(j, l1, 0.0D).tex(1.0D, 0.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(i, l1, 0.0D).tex(0.0D, 0.0D).color(192, 192, 192, 100).endVertex();
+                tessellator.draw();
+                vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+                vertexbuffer.pos(i, l1 + k1 - 1, 0.0D).tex(0.0D, 1.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(j - 1, l1 + k1 - 1, 0.0D).tex(1.0D, 1.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(j - 1, l1, 0.0D).tex(1.0D, 0.0D).color(192, 192, 192, 100).endVertex();
+                vertexbuffer.pos(i, l1, 0.0D).tex(0.0D, 0.0D).color(192, 192, 192, 100).endVertex();
+                tessellator.draw();
+            }
+            this.renderDecorations(mouseX, mouseY);
+            GlStateManager.enableTexture2D();
+            GlStateManager.shadeModel(7424);
+            GlStateManager.enableAlpha();
+            GlStateManager.disableBlend();
+        }
     }
 }
