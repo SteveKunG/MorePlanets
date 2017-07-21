f346f4cf7f28286ba95be4592e7fe739679231ec
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-21 19:48:11 +0700

    1.10.2, 1.11.2: Change Log GUI can use Enter key to exit

--- 1.10.2/src/main/java/stevekung/mods/moreplanets/client/gui/GuiFullChangeLog.java
+++ 1.10.2/src/main/java/stevekung/mods/moreplanets/client/gui/GuiFullChangeLog.java
@@ -98,7 +98,7 @@ public class GuiFullChangeLog extends GuiScreen
     @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException
     {
-        if (keyCode == 1)
+        if (keyCode == 1 || keyCode == 28)
         {
             this.mc.displayGuiScreen((GuiScreen)null);
         }
