fdc0c3766e28a093006996152d85bd714f69b37c
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-28 14:52:33 +0700

    Update build.gradle

--- 1.7.10/curseforge.bat
+++ 1.7.10/curseforge.bat
@@ -2,4 +2,4 @@ set GRADLE_USER_HOME=D:\.M\Minecraft Modding Workspace\.gradle_data\1.7.10_galac
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
 set PATH=C:\Program Files\Java\jdk1.7.0_80\bin
 set CLASSPATH=C:\Program Files\Java\jdk1.7.0_80\lib
-gradlew setupDecompWorkspace eclipse
+gradlew curseforge
