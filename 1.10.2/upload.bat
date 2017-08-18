2f5cf248273f0712adebcda0c122a6ccef4f2cb9
SteveKunG <mccommander_minecraft@hotmail.com>
2017-08-18 21:20:03 +0700

    Update workspace :D

--- 1.10.2/upload.bat
+++ 1.10.2/upload.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=D:\.M\Minecraft Modding Workspace\.gradle_data\1.10.2
+set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\MorePlanets\1.10.2\.gradle_data
 gradlew curseforge
--- 1.11.2/setup.bat
+++ 1.11.2/setup.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=D:\.M\Minecraft Modding Workspace\.gradle_data\1.11.2
+set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\MorePlanets\1.11.2\.gradle_data
 gradlew setupDecompWorkspace eclipse
