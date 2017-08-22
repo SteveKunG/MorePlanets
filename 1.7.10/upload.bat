4e1fddc746480f123fbc4bdbf354153add0d34b3
SteveKunG <mccommander_minecraft@hotmail.com>
2017-08-23 01:35:37 +0700

    1.7.10: Add Java 8 support

--- 1.7.10/upload.bat
+++ 1.7.10/upload.bat
@@ -1,5 +1,2 @@
 set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.7.10\.gradle_data
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
-set PATH=C:\Program Files\Java\jdk1.7.0_80\bin
-set CLASSPATH=C:\Program Files\Java\jdk1.7.0_80\lib
 gradlew curseforge
