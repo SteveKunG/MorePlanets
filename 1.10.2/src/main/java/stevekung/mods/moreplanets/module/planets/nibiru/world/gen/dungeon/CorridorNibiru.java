5c12d008987350231fc1396f95685789d43a121f
SteveKunG <mccommander_minecraft@hotmail.com>
2017-10-25 19:15:11 +0700

    1.10.2: Remove unchecked

--- 1.10.2/src/main/java/stevekung/mods/moreplanets/module/planets/nibiru/world/gen/dungeon/CorridorNibiru.java
+++ 1.10.2/src/main/java/stevekung/mods/moreplanets/module/planets/nibiru/world/gen/dungeon/CorridorNibiru.java
@@ -78,7 +78,6 @@ public class CorridorNibiru extends SizedPieceNibiru
         return true;
     }
 
-    @SuppressWarnings("unchecked")
     private <T extends SizedPieceNibiru> T getRoom(Class<?> clazz, DungeonStartNibiru startPiece, Random rand)
     {
         try
