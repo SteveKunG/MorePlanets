b26b8329ee42076e2a139a18a6dc706a4d1b7653
SteveKunG <mccommander_minecraft@hotmail.com>
2017-12-18 15:05:01 +0700

    Remove files

--- 1.10.2/build.bat
+++
@@ -1,2 +0,0 @@
-set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.10.2\.gradle_data
-gradlew buildJar
--- 1.10.2/src/main/java/stevekung/mods/moreplanets/util/dimension/IDarkEnergyProvider.java
+++
@@ -1,9 +0,0 @@
-package stevekung.mods.moreplanets.util.dimension;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public interface IDarkEnergyProvider
-{
-    public int getDarkEnergyMultiplier(World world, BlockPos pos);
-}
