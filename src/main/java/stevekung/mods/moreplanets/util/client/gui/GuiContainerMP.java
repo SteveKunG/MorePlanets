0d0a61f00c9ebe2a4e7076b0c6d3d12585a2b13d
SteveKunG <mccommander_minecraft@hotmail.com>
2018-01-12 17:03:10 +0700

    Fix Item tooltip rendering

--- src/main/java/stevekung/mods/moreplanets/util/client/gui/GuiContainerMP.java
+++ src/main/java/stevekung/mods/moreplanets/util/client/gui/GuiContainerMP.java
@@ -32,8 +32,8 @@ public abstract class GuiContainerMP extends GuiContainer
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
         this.drawDefaultBackground();
-
         super.drawScreen(mouseX, mouseY, partialTicks);
+        this.renderHoveredToolTip(mouseX, mouseY);
 
         if (this.renderInfo)
         {
