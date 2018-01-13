d51435b4e9dcfc45c539e4f15527f305b37349c9
SteveKunG <mccommander_minecraft@hotmail.com>
2018-01-13 19:52:22 +0700

    Added Capability (WIP)

---
+++ src/main/java/stevekung/mods/moreplanets/util/debug/MPCapData.java
@@ -0,0 +1,33 @@
+package stevekung.mods.moreplanets.util.debug;
+
+import net.minecraft.nbt.NBTTagCompound;
+import stevekung.mods.moreplanets.core.config.ConfigManagerMP;
+
+public class MPCapData implements MorePlanetsCapabilityData
+{
+    private String startedCelestial = ConfigManagerMP.startedPlanet;
+
+    @Override
+    public void writeNBT(NBTTagCompound nbt)
+    {
+        nbt.setString("StartedCelestial", this.startedCelestial);
+    }
+
+    @Override
+    public void readNBT(NBTTagCompound nbt)
+    {
+        this.startedCelestial = nbt.getString("StartedCelestial");
+    }
+
+    @Override
+    public void setStartCelestial(String object)
+    {
+        this.startedCelestial = object;
+    }
+
+    @Override
+    public String getStartCelestial()
+    {
+        return this.startedCelestial;
+    }
+}
