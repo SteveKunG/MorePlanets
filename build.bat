60a066282d1f13b44ebaf64866c1260941a7d202
SteveKunG <mccommander_minecraft@hotmail.com>
2018-02-11 17:16:19 +0700

    Update workspace

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.8.9\.gradle_data
+set GRADLE_USER_HOME=M:\Modding\.gradle_data_1.8
 gradlew buildJar
--- build_upload.bat
+++ build_upload.bat
@@ -1,2 +1,2 @@
 set GRADLE_USER_HOME=M:\Modding\.gradle_data_1.8
-gradlew curseforge
+gradlew buildJar curseforge
