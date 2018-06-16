07b11489805a7e927bb3e802e7248bd1a4ffaa5a
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-16 18:26:01 +0700

    Delete old files

--- build_upload.bat
+++ build_upload.bat
@@ -1,2 +1,7 @@
+rd /s /q "M:\Modding\MorePlanets\1.13\build\classes"
+rd /s /q "M:\Modding\MorePlanets\1.13\build\dependency-cache"
+rd /s /q "M:\Modding\MorePlanets\1.13\build\libs"
+rd /s /q "M:\Modding\MorePlanets\1.13\build\resources"
+rd /s /q "M:\Modding\MorePlanets\1.13\build\sources"
 set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.13\.gradle_data
 gradlew buildJar curseforge
