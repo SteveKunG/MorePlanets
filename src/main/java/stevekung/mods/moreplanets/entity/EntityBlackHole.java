81b6c74abad242b501dda4c1a2b3a15e14441d14
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-28 21:16:04 +0700

    Some fixes to Black Hole not spawning falling block

--- src/main/java/stevekung/mods/moreplanets/entity/EntityBlackHole.java
+++ src/main/java/stevekung/mods/moreplanets/entity/EntityBlackHole.java
@@ -3,14 +3,10 @@ package stevekung.mods.moreplanets.entity;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.item.EntityFallingBlock;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.MobEffects;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -51,41 +47,28 @@ public class EntityBlackHole extends Entity
         this.prevPosZ = this.posZ;
         this.move(MoverType.SELF, 0.0D, 0.0D, 0.0D);
         int range = this.getRange();
+        double nearestRange = 0.1D;
 
-        this.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(this.posX - range, this.posY - range, this.posZ - range, this.posX + range, this.posY + range, this.posZ + range)).forEach(entity ->
+        for (Entity entity : this.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(this.posX - range, this.posY - range, this.posZ - range, this.posX + range, this.posY + range, this.posZ + range)))
         {
-            if (!(entity instanceof EntityPlayer && ((EntityPlayer)entity).capabilities.isCreativeMode) && !(entity instanceof IImmuneBlackHole && ((IImmuneBlackHole)entity).isImmune()))
+            this.applyGravityToEntities(0.4F, 5000, 2500, 0.3F, range, entity, this.posX - 0.5D, this.posY, this.posZ - 0.5D);
+
+            for (Entity nearest : this.world.getEntitiesWithinAABB(entity.getClass(), new AxisAlignedBB(this.posX - nearestRange, this.posY - nearestRange, this.posZ - nearestRange, this.posX + nearestRange, this.posY + nearestRange, this.posZ + nearestRange)))
             {
-                double motionX = this.posX - entity.posX;
-                double motionY = this.posY - entity.posY;
-                double motionZ = this.posZ - entity.posZ;
-
-                if (entity instanceof EntityItem || entity instanceof EntityFallingBlock)
+                if (nearest instanceof EntityPlayer && ((EntityPlayer)nearest).capabilities.isCreativeMode || nearest instanceof IImmuneBlackHole && ((IImmuneBlackHole)nearest).isImmune())
                 {
-                    motionY = this.posY - entity.posY + 0.5D;
+                    return;
                 }
-
-                entity.motionX = motionX * this.getPullSpeed();
-                entity.motionY = motionY * this.getPullSpeed();
-                entity.motionZ = motionZ * this.getPullSpeed();
-
-                this.world.getEntitiesWithinAABB(entity.getClass(), new AxisAlignedBB(this.posX - 0.25D, this.posY - 0.25D, this.posZ - 0.25D, this.posX + 0.25D, this.posY + 0.5D, this.posZ + 0.25D)).forEach(nearest ->
+                if (nearest instanceof EntityPlayer)
                 {
-                    if (nearest instanceof EntityLivingBase)
-                    {
-                        ((EntityLivingBase)nearest).addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 32767, 0, false, false));
-                    }
-                    if (nearest instanceof EntityPlayer)
-                    {
-                        nearest.attackEntityFrom(DamageSourceMP.BLACK_HOLE, 10.0F);
-                    }
-                    if (!(nearest instanceof EntityPlayer) && !(nearest instanceof EntityBlackHole))
-                    {
-                        nearest.setDead();
-                    }
-                });
+                    nearest.attackEntityFrom(DamageSourceMP.BLACK_HOLE, 10.0F);
+                }
+                if (!(nearest instanceof EntityPlayer) && !(nearest instanceof EntityBlackHole))
+                {
+                    nearest.setDead();
+                }
             }
-        });
+        }
 
         if (this.lifeTick == 0)
         {
@@ -137,47 +120,6 @@ public class EntityBlackHole extends Entity
         }
     }
 
-    private void spawnFallingBlock()
-    {
-        int blockPosX = MathHelper.floor(this.posX);
-        int blockPosY = MathHelper.floor(this.posY);
-        int blockPosZ = MathHelper.floor(this.posZ);
-        int radius = 1 + this.spawnBlockRadiusTick;
-
-        for (int x = -radius; x < radius; x++)
-        {
-            for (int y = -radius; y < radius; y++)
-            {
-                for (int z = -radius; z < radius; z++)
-                {
-                    double dist = MathHelper.sqrt(x * x + y * y + z * z);
-
-                    if (dist <= radius)
-                    {
-                        BlockPos pos = new BlockPos(blockPosX + x, blockPosY + y, blockPosZ + z);
-                        IBlockState state = this.world.getBlockState(pos);
-                        Block block = state.getBlock();
-
-                        if (block.getExtendedState(state, this.world, pos) != null && radius <= 15)
-                        {
-                            this.world.setBlockToAir(pos);
-                        }
-                        if (!block.isAir(state, this.world, pos) && block.getExtendedState(state, this.world, pos) == state)
-                        {
-                            this.world.setBlockToAir(pos);
-                            EntityFallingBlock fallingBlock = new EntityFallingBlock(this.world, blockPosX + x, blockPosY + y, blockPosZ + z, state);
-                            fallingBlock.fallTime = 1;
-                            fallingBlock.shouldDropItem = false;
-                            fallingBlock.motionY += 0.5D;
-                            fallingBlock.setLocationAndAngles(blockPosX + x + 0.5D, blockPosY + y, blockPosZ + z + 0.5D, 0.0F, 0.0F);
-                            this.world.spawnEntity(fallingBlock);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     @Override
     public void setDead()
     {
@@ -215,5 +157,102 @@ public class EntityBlackHole extends Entity
     protected double getPullSpeed()
     {
         return 0.2D;
+    }
+
+    private void spawnFallingBlock()
+    {
+        int blockPosX = MathHelper.floor(this.posX);
+        int blockPosY = MathHelper.floor(this.posY);
+        int blockPosZ = MathHelper.floor(this.posZ);
+        int radius = 1 + this.spawnBlockRadiusTick;
+
+        for (int x = -radius; x < radius; x++)
+        {
+            for (int y = -radius; y < radius; y++)
+            {
+                for (int z = -radius; z < radius; z++)
+                {
+                    double dist = MathHelper.sqrt(x * x + y * y + z * z);
+
+                    if (dist <= radius)
+                    {
+                        BlockPos pos = new BlockPos(blockPosX + x, blockPosY + y, blockPosZ + z);
+                        IBlockState state = this.world.getBlockState(pos);
+                        Block block = state.getBlock();
+
+                        if (!block.isAir(state, this.world, pos))
+                        {
+                            this.world.setBlockToAir(pos);
+                            EntityFallingBlock fallingBlock = new EntityFallingBlock(this.world, blockPosX + x, blockPosY + y, blockPosZ + z, state);
+                            fallingBlock.fallTime = 1;
+                            fallingBlock.shouldDropItem = false;
+                            fallingBlock.motionY += 0.5D;
+                            fallingBlock.setLocationAndAngles(blockPosX + x + 0.5D, blockPosY + y, blockPosZ + z + 0.5D, 0.0F, 0.0F);
+                            this.world.spawnEntity(fallingBlock);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void applyGravityToEntities(float gravStrength, float maxGravXZ, float maxGravY, float minGrav, float range, Entity entity, double xCoord, double yCoord, double zCoord)
+    {
+        // distance formula
+        double dist = Math.sqrt(Math.pow(xCoord + 0.5 - entity.posX, 2) + Math.pow(zCoord + 0.5 - entity.posZ, 2) + Math.pow(yCoord + 0.5 - entity.posY, 2));
+
+        if (dist > range)
+        {
+            return;
+        }
+
+        double xDisplacment = entity.posX - (xCoord + 0.5);
+        double yDisplacment = entity.posY - (yCoord + 0.5);
+        double zDisplacment = entity.posZ - (zCoord + 0.5);
+        // http://en.wikipedia.org/wiki/Spherical_coordinate_system#Coordinate_system_conversions
+        double theta = Math.acos(zDisplacment / dist);
+        double phi = Math.atan2(yDisplacment, xDisplacment);
+        // Gravity decreases linearly
+        double gravForce = gravStrength * (1 - dist / range);
+
+        if (entity instanceof EntityPlayer && ((EntityPlayer)entity).capabilities.isCreativeMode || entity instanceof IImmuneBlackHole && ((IImmuneBlackHole)entity).isImmune())
+        {
+            return;
+        }
+
+        gravForce *= 2.0D;
+
+        double vecX = -gravForce * Math.sin(theta) * Math.cos(phi);
+        double vecY = -gravForce * Math.sin(theta) * Math.sin(phi);
+        double vecZ = -gravForce * Math.cos(theta);
+
+        // trims gravity above max
+        if (Math.abs(vecX) > maxGravXZ)
+        {
+            vecX *= maxGravXZ / Math.abs(vecX);
+        }
+        if (Math.abs(vecY) > maxGravY)
+        {
+            vecY *= maxGravY / Math.abs(vecY);
+        }
+        if (Math.abs(vecZ) > maxGravXZ)
+        {
+            vecZ *= maxGravXZ / Math.abs(vecZ);
+        }
+
+        // trims gravity below min
+        if (Math.abs(vecX) < minGrav)
+        {
+            vecX = 0;
+        }
+        if (Math.abs(vecY) < minGrav)
+        {
+            vecY = 0;
+        }
+        if (Math.abs(vecZ) < minGrav)
+        {
+            vecZ = 0;
+        }
+        entity.setVelocity(entity.motionX + vecX, entity.motionY + vecY, entity.motionZ + vecZ);
     }
 }
