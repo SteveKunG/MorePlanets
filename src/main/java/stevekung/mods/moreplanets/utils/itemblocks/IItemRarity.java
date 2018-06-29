ea31961c89bf6014d9e394a6c490c2bd66bb66d1
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-29 14:13:37 +0700

    Remove unused rarity color

--- src/main/java/stevekung/mods/moreplanets/utils/itemblocks/IItemRarity.java
+++ src/main/java/stevekung/mods/moreplanets/utils/itemblocks/IItemRarity.java
@@ -4,7 +4,6 @@ import stevekung.mods.stevekunglib.utils.ColorUtils;
 
 public interface IItemRarity
 {
-    public static final String DEFAULT = "85, 85, 255";
     public static final String SPECIAL = "0, 255, 207";
     public static final String ALIEN = "178, 109, 237";
     public static final String COMMON = "117, 151, 255";
