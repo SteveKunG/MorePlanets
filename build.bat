442135a9af6d8c574e096ee6fb673d6e8999b28f
SteveKunG <mccommander_minecraft@hotmail.com>
2018-09-13 22:04:02 +0700

    Update workspace

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-rd /s /q "M:\Modding\MorePlanets\1.13\build\classes"
-rd /s /q "M:\Modding\MorePlanets\1.13\build\dependency-cache"
-rd /s /q "M:\Modding\MorePlanets\1.13\build\libs"
-rd /s /q "M:\Modding\MorePlanets\1.13\build\resources"
-rd /s /q "M:\Modding\MorePlanets\1.13\build\sources"
-set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.13\.gradle_data
+rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\classes"
+rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\dependency-cache"
+rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\libs"
+rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\resources"
+rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\sources"
+set GRADLE_USER_HOME=M:\Modding\MorePlanets\1.13-pre\.gradle_data
 gradlew buildJar
