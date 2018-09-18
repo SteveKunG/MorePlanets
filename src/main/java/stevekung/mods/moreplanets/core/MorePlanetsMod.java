2f47992333994f0fc965b243743d4663b343bdaa
SteveKunG <mccommander_minecraft@hotmail.com>
2018-09-18 15:14:59 +0700

    Bump to 2.1.4

--- src/main/java/stevekung/mods/moreplanets/core/MorePlanetsMod.java
+++ src/main/java/stevekung/mods/moreplanets/core/MorePlanetsMod.java
@@ -50,7 +50,7 @@ public class MorePlanetsMod
     public static final String MOD_ID = "moreplanets";
     private static final int MAJOR_VERSION = 2;
     private static final int MINOR_VERSION = 1;
-    private static final int BUILD_VERSION = 3;
+    private static final int BUILD_VERSION = 4;
     public static final String VERSION = MorePlanetsMod.MAJOR_VERSION + "." + MorePlanetsMod.MINOR_VERSION + "." + MorePlanetsMod.BUILD_VERSION;
     private static final String FORGE_VERSION = "after:forge@[14.23.4.2705,); ";
     private static final String DEPENDENCIES = "after:jei@[4.10.0.200,); ";
