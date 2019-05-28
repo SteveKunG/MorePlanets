7a703fb21f9164ff6a3679e8d0b4e2732b6aba95
SteveKunG <mccommander_minecraft@hotmail.com>
2019-05-28 09:08:26 +0700

    Update gradle

--- build_upload.bat
+++ build_upload.bat
@@ -1,7 +1,2 @@
-rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\classes"
-rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\dependency-cache"
-rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\libs"
-rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\resources"
-rd /s /q "M:\Modding\MorePlanets\1.13-pre\build\sources"
-set GRADLE_USER_HOME=C:\Users\SteveKunG\.gradle_eclipse
+set GRADLE_USER_HOME=C:\Users\SteveKunG\.gradle_1.12.2
 gradlew buildJar curseforge
