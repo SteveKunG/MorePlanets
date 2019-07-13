10529bca367480803bf20977c6e76fc9626c40db
SteveKunG <mccommander_minecraft@hotmail.com>
2019-07-14 05:00:14 +0700

    Using WeatherTickEvent instead WorldTickEvent

--- src/main/java/stevekung/mods/moreplanets/core/event/WorldTickEventHandler.java
+++ src/main/java/stevekung/mods/moreplanets/core/event/WorldTickEventHandler.java
@@ -1,25 +1,17 @@
 package stevekung.mods.moreplanets.core.event;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 import micdoodle8.mods.galacticraft.core.util.WorldUtil;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
-import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
-import net.minecraftforge.fml.relauncher.Side;
 import stevekung.mods.moreplanets.core.config.ConfigManagerMP;
 import stevekung.mods.moreplanets.core.data.WorldDataSurvivalPlanet;
 import stevekung.mods.moreplanets.init.MPBiomes;
@@ -31,6 +23,7 @@ import stevekung.mods.moreplanets.planets.nibiru.entity.weather.EntityNibiruLigh
 import stevekung.mods.moreplanets.planets.nibiru.world.gen.biome.BiomeInfectedBadlands;
 import stevekung.mods.moreplanets.planets.nibiru.world.gen.biome.BiomeInfectedDesert;
 import stevekung.mods.moreplanets.planets.nibiru.world.gen.biome.BiomeInfectedMountains;
+import stevekung.mods.stevekunglib.utils.event.WeatherTickEvent;
 
 public class WorldTickEventHandler
 {
@@ -50,117 +43,77 @@ public class WorldTickEventHandler
     }
 
     @SubscribeEvent
-    public void onWorldTick(WorldTickEvent event)
+    public void onWeatherTick(WeatherTickEvent event) //TODO Fix entity spawning outside unloaded chunk
     {
-        World world = event.world;
+        World world = event.getWorld();
+        BlockPos pos = event.getStrikePos();
         this.updateLCG = this.updateLCG * 3 + 1013904223;
         int l = this.updateLCG >> 2;
 
-        if (event.side == Side.SERVER && event.phase == Phase.START) //TODO Check entity spawning outside unloaded chunk
+        if (DimensionManager.getWorld(ConfigManagerMP.moreplanets_dimension.idDimensionDiona) != null)
         {
-            if (DimensionManager.getWorld(ConfigManagerMP.moreplanets_dimension.idDimensionDiona) != null)
+            if (world.provider instanceof WorldProviderDiona)
             {
-                for (Iterator<Chunk> iterator = world.getPersistentChunkIterable(((WorldServer)world).getPlayerChunkMap().getChunkIterator()); iterator.hasNext();)
+                if (this.canBeamStrike(world, pos) && world.rand.nextInt(75000) == 0)
                 {
-                    Chunk chunk = iterator.next();
-                    int x = chunk.x * 16;
-                    int z = chunk.z * 16;
-                    BlockPos strikePos = new BlockPos(x + (l & 15), 0, z + (l >> 8 & 15));
-                    BlockPos pos = this.adjustPosToNearbyEntity(world, strikePos);
+                    EntityAlienBeam beam = new EntityAlienBeam(world);
+                    beam.setLocationAndAngles(pos.getX(), pos.getY(), pos.getZ(), 0.0F, 0.0F);
 
-                    if (world.provider instanceof WorldProviderDiona)
+                    if (world.isBlockLoaded(pos))
                     {
-                        if (this.canBeamStrike(world, pos) && world.rand.nextInt(75000) == 0)
-                        {
-                            EntityAlienBeam beam = new EntityAlienBeam(world);
-                            beam.setLocationAndAngles(pos.getX(), pos.getY(), pos.getZ(), 0.0F, 0.0F);
-
-                            if (world.isBlockLoaded(pos))
-                            {
-                                world.spawnEntity(beam);
-                            }
-                        }
-                    }
-                }
-            }
-            else if (DimensionManager.getWorld(ConfigManagerMP.moreplanets_dimension.idDimensionNibiru) != null)
-            {
-                for (Iterator<Chunk> iterator = world.getPersistentChunkIterable(((WorldServer)world).getPlayerChunkMap().getChunkIterator()); iterator.hasNext();)
-                {
-                    Chunk chunk = iterator.next();
-                    int x = chunk.x * 16;
-                    int z = chunk.z * 16;
-                    BlockPos strikePos = new BlockPos(x + (l & 15), 0, z + (l >> 8 & 15));
-                    BlockPos pos = this.adjustPosToNearbyEntity(world, strikePos);
-
-                    if (world.provider instanceof WorldProviderNibiru)
-                    {
-                        boolean raining = world.isRaining();
-                        boolean thunder = world.isThundering();
-                        Biome biome = world.getBiome(pos);
-                        EntityNibiruLightningBolt bolt = new EntityNibiruLightningBolt(world, pos.getX(), pos.getY(), pos.getZ(), false);
-                        EntityNibiruLightningBolt boltFire = new EntityNibiruLightningBolt(world, pos.getX(), pos.getY(), pos.getZ(), true);
-
-                        if (world.provider.canDoLightning(chunk) && raining && thunder && world.rand.nextInt(1000) == 0)
-                        {
-                            if (world.isRainingAt(pos))
-                            {
-                                world.spawnEntity(boltFire);
-                            }
-                        }
-                        if (world.rand.nextInt(16) == 0)
-                        {
-                            BlockPos pos1 = pos.down();
-
-                            if (world.isAreaLoaded(pos1, 1))
-                            {
-                                if (world.canBlockFreezeNoWater(pos1))
-                                {
-                                    world.setBlockState(pos1, MPBlocks.INFECTED_ICE.getDefaultState());
-                                }
-                            }
-                            if (raining)
-                            {
-                                if (world.canSnowAt(pos, true))
-                                {
-                                    world.setBlockState(pos, biome == MPBiomes.COLD_GREEN_VEIN_MOUTAINS ? MPBlocks.PURIFIED_SNOW_LAYER.getDefaultState() : MPBlocks.INFECTED_SNOW_LAYER.getDefaultState());
-                                }
-                                if (world.getBiome(pos1).canRain())
-                                {
-                                    world.getBlockState(pos1).getBlock().fillWithRain(world, pos1);
-                                }
-                            }
-                        }
-                        if (biome instanceof BiomeInfectedDesert || biome instanceof BiomeInfectedMountains || biome instanceof BiomeInfectedBadlands)
-                        {
-                            if (world.rand.nextInt(250000) == 0)
-                            {
-                                world.spawnEntity(bolt);
-                            }
-                        }
+                        world.spawnEntity(beam);
                     }
                 }
             }
         }
-    }
-
-    private BlockPos adjustPosToNearbyEntity(World world, BlockPos pos)
-    {
-        BlockPos pos1 = world.getPrecipitationHeight(pos);
-        AxisAlignedBB aabb = new AxisAlignedBB(pos1, new BlockPos(pos1.getX(), world.getHeight(), pos1.getZ())).grow(3.0D);
-        List<EntityLivingBase> list = world.getEntitiesWithinAABB(EntityLivingBase.class, aabb, entity -> entity != null && entity.isEntityAlive() && world.canSeeSky(entity.getPosition()));
-
-        if (!list.isEmpty())
+        else if (DimensionManager.getWorld(ConfigManagerMP.moreplanets_dimension.idDimensionNibiru) != null)
         {
-            return list.get(world.rand.nextInt(list.size())).getPosition();
-        }
-        else
-        {
-            if (pos1.getY() == -1)
+            if (world.provider instanceof WorldProviderNibiru)
             {
-                pos1 = pos1.up(2);
+                boolean raining = world.isRaining();
+                boolean thunder = world.isThundering();
+                Biome biome = world.getBiome(pos);
+                EntityNibiruLightningBolt bolt = new EntityNibiruLightningBolt(world, pos.getX(), pos.getY(), pos.getZ(), false);
+                EntityNibiruLightningBolt boltFire = new EntityNibiruLightningBolt(world, pos.getX(), pos.getY(), pos.getZ(), true);
+
+                if (world.provider.canDoLightning(new Chunk(world, event.getChunkX(), event.getChunkZ())) && raining && thunder && world.rand.nextInt(1000) == 0)
+                {
+                    if (world.isRainingAt(pos))
+                    {
+                        world.spawnEntity(boltFire);
+                    }
+                }
+                if (world.rand.nextInt(16) == 0)
+                {
+                    BlockPos pos1 = pos.down();
+
+                    if (world.isAreaLoaded(pos1, 1))
+                    {
+                        if (world.canBlockFreezeNoWater(pos1))
+                        {
+                            world.setBlockState(pos1, MPBlocks.INFECTED_ICE.getDefaultState());
+                        }
+                    }
+                    if (raining)
+                    {
+                        if (world.canSnowAt(pos, true))
+                        {
+                            world.setBlockState(pos, biome == MPBiomes.COLD_GREEN_VEIN_MOUTAINS ? MPBlocks.PURIFIED_SNOW_LAYER.getDefaultState() : MPBlocks.INFECTED_SNOW_LAYER.getDefaultState());
+                        }
+                        if (world.getBiome(pos1).canRain())
+                        {
+                            world.getBlockState(pos1).getBlock().fillWithRain(world, pos1);
+                        }
+                    }
+                }
+                if (biome instanceof BiomeInfectedDesert || biome instanceof BiomeInfectedMountains || biome instanceof BiomeInfectedBadlands)
+                {
+                    if (world.rand.nextInt(250000) == 0)
+                    {
+                        world.spawnEntity(bolt);
+                    }
+                }
             }
-            return pos1;
         }
     }
 
