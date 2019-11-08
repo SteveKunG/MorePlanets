ef7e63792ca93faf09260d9b692d7f6f2ac00da2
SteveKunG <mccommander_minecraft@hotmail.com>
2019-11-08 11:59:21 +0700

    Added backward compatibility for Diona Dungeon Spawner

--- src/main/java/stevekung/mods/moreplanets/planets/diona/tileentity/TileEntityDionaDungeonSpawner.java
+++ src/main/java/stevekung/mods/moreplanets/planets/diona/tileentity/TileEntityDionaDungeonSpawner.java
@@ -1,5 +1,6 @@
 package stevekung.mods.moreplanets.planets.diona.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
 import stevekung.mods.moreplanets.planets.diona.entity.EntityInfectedPurloniteSlimeBoss;
 import stevekung.mods.moreplanets.utils.tileentity.TileEntityDungeonSpawnerMP;
 
@@ -8,5 +9,30 @@ public class TileEntityDionaDungeonSpawner extends TileEntityDungeonSpawnerMP<En
     public TileEntityDionaDungeonSpawner()
     {
         super(EntityInfectedPurloniteSlimeBoss.class);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt)
+    {
+        // backward compatibility
+        String bossClass = nbt.getString("bossClass");
+
+        if (bossClass.equals("stevekung.mods.moreplanets.planets.diona.entity.RenderInfectedCrystallizedSlimeBoss"))
+        {
+            bossClass = "stevekung.mods.moreplanets.planets.diona.entity.RenderInfectedPurloniteSlimeBoss";
+        }
+
+        try
+        {
+            this.bossClass = (Class<EntityInfectedPurloniteSlimeBoss>) Class.forName(bossClass);
+        }
+        catch (Exception e)
+        {
+            if (!this.world.isRemote)
+            {
+                e.printStackTrace();
+            }
+        }
+        super.readFromNBT(nbt);
     }
 }
