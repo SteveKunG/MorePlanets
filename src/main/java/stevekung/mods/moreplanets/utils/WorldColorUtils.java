242f8b1a0a80f6f8b005619a4c5b2049be0aa952
SteveKunG <mccommander_minecraft@hotmail.com>
2020-11-16 18:27:21 +0700

    Switch to use Vec3d to prevent class loading issue

--- src/main/java/stevekung/mods/moreplanets/utils/WorldColorUtils.java
+++ src/main/java/stevekung/mods/moreplanets/utils/WorldColorUtils.java
@@ -1,22 +1,22 @@
 package stevekung.mods.moreplanets.utils;
 
-import micdoodle8.mods.galacticraft.api.vector.Vector3;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import stevekung.mods.moreplanets.moons.koentus.dimension.WorldProviderKoentus;
 import stevekung.mods.moreplanets.planets.chalos.dimension.WorldProviderChalos;
 
 public class WorldColorUtils
 {
-    public static Vector3 getWorldColor(World world)
+    public static Vec3d getWorldColor(World world)
     {
         if (world.provider instanceof WorldProviderChalos)
         {
-            return new Vector3(0.9F, 0.85F, 0.65F);
+            return new Vec3d(0.9F, 0.85F, 0.65F);
         }
         else if (world.provider instanceof WorldProviderKoentus)
         {
-            return new Vector3(0.7F, 0.85F, 1.0F);
+            return new Vec3d(0.7F, 0.85F, 1.0F);
         }
-        return new Vector3(1.0F, 1.0F, 1.0F);
+        return new Vec3d(1.0F, 1.0F, 1.0F);
     }
 }
