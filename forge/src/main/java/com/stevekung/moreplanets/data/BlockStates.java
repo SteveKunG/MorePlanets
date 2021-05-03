dba4fe725010ca9ca0badeecfb17544c0c788f7d
SteveKunG <mccommander_minecraft@hotmail.com>
2021-05-03 14:24:42 +0700

    Update Diona Regolth and Stone model

--- forge/src/main/java/com/stevekung/moreplanets/data/BlockStates.java
+++ forge/src/main/java/com/stevekung/moreplanets/data/BlockStates.java
@@ -1,8 +1,11 @@
 package com.stevekung.moreplanets.data;
 
+import com.google.common.collect.ObjectArrays;
 import com.stevekung.moreplanets.world.level.block.MPBlocks;
 import com.stevekung.stevekungslib.data.BlockStateProviderBase;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class BlockStates extends BlockStateProviderBase
@@ -15,9 +18,9 @@ public class BlockStates extends BlockStateProviderBase
     @Override
     protected void registerStatesAndModels()
     {
-        this.simpleBlock(MPBlocks.DIONA_REGOLITH);
+        this.simpleBlock(MPBlocks.DIONA_REGOLITH, ConfiguredModel.allYRotations(this.models().cubeBottomTop("diona_regolith", this.modLoc("block/diona_regolith_side"), this.modLoc("block/diona_fine_regolith"), this.modLoc("block/diona_regolith_top")), 0, false));
         this.simpleBlock(MPBlocks.DIONA_FINE_REGOLITH);
-        this.simpleBlock(MPBlocks.DIONA_STONE);
+        this.simpleBlock(MPBlocks.DIONA_STONE, model -> ObjectArrays.concat(ConfiguredModel.allYRotations(model, 0, false), ConfiguredModel.allYRotations(model, 180, false), ConfiguredModel.class));
         this.simpleBlock(MPBlocks.DIONA_COBBLESTONE);
         this.simpleBlock(MPBlocks.GLOWING_IRON_BLOCK);
         this.simpleBlock(MPBlocks.RAW_GLOWING_IRON_BLOCK);
