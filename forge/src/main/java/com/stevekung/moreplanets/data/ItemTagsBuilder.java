a0190ad1a1db4baa29c554442cab811a304ac5b0
SteveKunG <mccommander_minecraft@hotmail.com>
2021-05-13 16:14:25 +0700

    Added tools tags for fabric item tags

--- forge/src/main/java/com/stevekung/moreplanets/data/ItemTagsBuilder.java
+++ forge/src/main/java/com/stevekung/moreplanets/data/ItemTagsBuilder.java
@@ -2,15 +2,24 @@ package com.stevekung.moreplanets.data;
 
 import com.stevekung.moreplanets.world.item.MPItems;
 import com.stevekung.moreplanets.world.level.block.MPBlocks;
+import com.stevekung.stevekungslib.utils.TagUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class ItemTagsBuilder extends ItemTagsProvider
 {
+    private static final Tag.Named<Item> FABRIC_AXES = TagUtils.createItemTag("fabric", "axes");
+    private static final Tag.Named<Item> FABRIC_HOES = TagUtils.createItemTag("fabric", "hoes");
+    private static final Tag.Named<Item> FABRIC_PICKAXES = TagUtils.createItemTag("fabric", "pickaxes");
+    private static final Tag.Named<Item> FABRIC_SHOVELS = TagUtils.createItemTag("fabric", "shovels");
+    private static final Tag.Named<Item> FABRIC_SWORDS = TagUtils.createItemTag("fabric", "swords");
+
     public ItemTagsBuilder(DataGenerator generator, BlockTagsProvider blockTagProvider, String modid, ExistingFileHelper helper)
     {
         super(generator, blockTagProvider, modid, helper);
@@ -26,5 +35,11 @@ public class ItemTagsBuilder extends ItemTagsProvider
         this.tag(Tags.Items.STORAGE_BLOCKS_IRON).add(MPBlocks.GLOWING_IRON_BLOCK.asItem());
 
         this.tag(Tags.Items.INGOTS_IRON).add(MPItems.GLOWING_IRON_INGOT);
+
+        this.tag(FABRIC_AXES).add(MPItems.GLOWING_IRON_AXE);
+        this.tag(FABRIC_HOES).add(MPItems.GLOWING_IRON_HOE);
+        this.tag(FABRIC_PICKAXES).add(MPItems.GLOWING_IRON_PICKAXE);
+        this.tag(FABRIC_SHOVELS).add(MPItems.GLOWING_IRON_SHOVEL);
+        this.tag(FABRIC_SWORDS).add(MPItems.GLOWING_IRON_SWORD);
     }
 }
