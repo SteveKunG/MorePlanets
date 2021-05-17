51434b5e6a86e2154b7a1de2942fc60a794cbeeb
SteveKunG <mccommander_minecraft@hotmail.com>
2021-05-17 13:48:00 +0700

    Update Language data generator

--- forge/src/main/java/com/stevekung/moreplanets/data/Language.java
+++ forge/src/main/java/com/stevekung/moreplanets/data/Language.java
@@ -16,45 +16,45 @@ public class Language extends LanguageProviderBase
     @Override
     protected void addTranslations()
     {
-        this.add(MPBlocks.DIONA_REGOLITH, "Diona Regolith");
-        this.add(MPBlocks.DIONA_FINE_REGOLITH, "Diona Fine Regolith");
-        this.add(MPBlocks.DIONA_STONE, "Diona Stone");
-        this.add(MPBlocks.DIONA_COBBLESTONE, "Diona Cobblestone");
+        this.add(MPBlocks.DIONA_REGOLITH);
+        this.add(MPBlocks.DIONA_FINE_REGOLITH);
+        this.add(MPBlocks.DIONA_STONE);
+        this.add(MPBlocks.DIONA_COBBLESTONE);
         this.add(MPBlocks.GLOWING_IRON_BLOCK, "Block of Glowing Iron");
         this.add(MPBlocks.RAW_GLOWING_IRON_BLOCK, "Block of Raw Glowing Iron");
-        this.add(MPBlocks.METEORIC_IRON_STABILIZER, "Meteoric Iron Stabilizer");
-        this.add(MPBlocks.ION_PLASMA_ROD, "Ion Plasma Rod");
+        this.add(MPBlocks.METEORIC_IRON_STABILIZER);
+        this.add(MPBlocks.ION_PLASMA_ROD);
         this.add(MPBlocks.PURLONITE_BLOCK, "Block of Purlonite");
-        this.add(MPBlocks.BUDDING_PURLONITE, "Budding Purlonite");
-        this.add(MPBlocks.PURLONITE_CLUSTER, "Purlonite Cluster");
-        this.add(MPBlocks.LARGE_PURLONITE_BUD, "Large Purlonite Bud");
-        this.add(MPBlocks.MEDIUM_PURLONITE_BUD, "Medium Purlonite Bud");
-        this.add(MPBlocks.SMALL_PURLONITE_BUD, "Small Purlonite Bud");
-        this.add(MPBlocks.PURLONITE_CRYSTAL_LANTERN, "Purlonite Crystal Lantern");
-        this.add(MPBlocks.DARK_CRYSTAL_LANTERN, "Dark Crystal Lantern");
-        this.add(MPBlocks.DARK_ENERGY_CORE, "Dark Energy Core");
-        this.add(MPBlocks.ZELIUS_EGG, "Zelius Egg");
-        this.add(MPBlocks.DARK_ENERGY_GENERATOR, "Dark Energy Generator");
+        this.add(MPBlocks.BUDDING_PURLONITE);
+        this.add(MPBlocks.PURLONITE_CLUSTER);
+        this.add(MPBlocks.LARGE_PURLONITE_BUD);
+        this.add(MPBlocks.MEDIUM_PURLONITE_BUD);
+        this.add(MPBlocks.SMALL_PURLONITE_BUD);
+        this.add(MPBlocks.PURLONITE_CRYSTAL_LANTERN);
+        this.add(MPBlocks.DARK_CRYSTAL_LANTERN);
+        this.add(MPBlocks.DARK_ENERGY_CORE);
+        this.add(MPBlocks.ZELIUS_EGG);
+        this.add(MPBlocks.DARK_ENERGY_GENERATOR);
         this.add(MPBlocks.COMPACTED_PURLONITE_BLOCK, "Compacted Block of Purlonite");
-        this.add(MPBlocks.DISPLAY_JAR, "Display Jar");
-        this.add(MPBlocks.PURLONITE_WORM_JAR, "Purlonite Worm Jar");
-        this.add(MPBlocks.CHALOS_SPORE_JAR, "Chalos Spore Jar");
+        this.add(MPBlocks.DISPLAY_JAR);
+        this.add(MPBlocks.PURLONITE_WORM_JAR);
+        this.add(MPBlocks.CHALOS_SPORE_JAR);
 
-        this.add(MPItems.GLOWING_IRON_INGOT, "Glowing Iron Ingot");
-        this.add(MPItems.RAW_GLOWING_IRON, "Raw Glowing Iron");
-        this.add(MPItems.PURLONITE_SHARD, "Purlonite Shard");
-        this.add(MPItems.DARK_CRYSTAL_SHARD, "Dark Crystal Shard");
+        this.add(MPItems.GLOWING_IRON_INGOT);
+        this.add(MPItems.RAW_GLOWING_IRON);
+        this.add(MPItems.PURLONITE_SHARD);
+        this.add(MPItems.DARK_CRYSTAL_SHARD);
 
-        this.add(MPItems.GLOWING_IRON_SWORD, "Glowing Iron Sword");
-        this.add(MPItems.GLOWING_IRON_SHOVEL, "Glowing Iron Shovel");
-        this.add(MPItems.GLOWING_IRON_PICKAXE, "Glowing Iron Pickaxe");
-        this.add(MPItems.GLOWING_IRON_AXE, "Glowing Iron Axe");
-        this.add(MPItems.GLOWING_IRON_HOE, "Glowing Iron Hoe");
+        this.add(MPItems.GLOWING_IRON_SWORD);
+        this.add(MPItems.GLOWING_IRON_SHOVEL);
+        this.add(MPItems.GLOWING_IRON_PICKAXE);
+        this.add(MPItems.GLOWING_IRON_AXE);
+        this.add(MPItems.GLOWING_IRON_HOE);
 
-        this.add(MPItems.GLOWING_IRON_HELMET, "Glowing Iron Helmet");
-        this.add(MPItems.GLOWING_IRON_CHESTPLATE, "Glowing Iron Chestplate");
-        this.add(MPItems.GLOWING_IRON_LEGGINGS, "Glowing Iron Leggings");
-        this.add(MPItems.GLOWING_IRON_BOOTS, "Glowing Iron Boots");
+        this.add(MPItems.GLOWING_IRON_HELMET);
+        this.add(MPItems.GLOWING_IRON_CHESTPLATE);
+        this.add(MPItems.GLOWING_IRON_LEGGINGS);
+        this.add(MPItems.GLOWING_IRON_BOOTS);
 
         this.add(MorePlanetsMod.TAB.getDisplayName().getString(), "More Planets");
     }
