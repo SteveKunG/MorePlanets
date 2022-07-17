abf66fbf7f37eeb4584fcccd6df58363cd6c0a56
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-07-18 01:39:16 +0700

    Temporary config

---
+++ src/main/java/com/stevekung/moreplanets/config/MorePlanetsConfig.java
@@ -0,0 +1,10 @@
+package com.stevekung.moreplanets.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = "more_planets")
+@Config.Gui.Background("minecraft:textures/block/deepslate.png")
+public final class MorePlanetsConfig implements ConfigData
+{
+}
--- src/main/java/com/stevekung/moreplanets/modmenu/ModMenuIntegrationMP.java
+++ src/main/java/com/stevekung/moreplanets/modmenu/ModMenuIntegrationMP.java
@@ -1,13 +1,16 @@
 package com.stevekung.moreplanets.modmenu;
 
+import com.stevekung.moreplanets.config.MorePlanetsConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import me.shedaniel.autoconfig.AutoConfig;
 
 public class ModMenuIntegrationMP implements ModMenuApi
 {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory()
     {
-        return screen -> screen;// TODO
+        return parent -> AutoConfig.getConfigScreen(MorePlanetsConfig.class, parent).get();
     }
 }
