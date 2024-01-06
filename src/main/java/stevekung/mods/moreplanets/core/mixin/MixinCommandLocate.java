f4c11b1d1ff1c956359e4586ab765209d5361a51
SteveKunG <mccommander_minecraft@hotmail.com>
2024-01-06 15:37:31 +0700

    Avoids using @Redirect

--- src/main/java/stevekung/mods/moreplanets/core/mixin/MixinCommandLocate.java
+++ src/main/java/stevekung/mods/moreplanets/core/mixin/MixinCommandLocate.java
@@ -1,27 +1,25 @@
 package stevekung.mods.moreplanets.core.mixin;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-import com.google.common.collect.Lists;
-
-import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandLocate;
 
 @Mixin(CommandLocate.class)
 public class MixinCommandLocate
 {
-    @Redirect(method = "getTabCompletions", at = @At(value = "INVOKE", target = "net/minecraft/command/CommandLocate.getListOfStringsMatchingLastWord([Ljava/lang/String;[Ljava/lang/String;)Ljava/util/List;"))
-    private List<String> moreplanets$addStructures(String[] args, String... possibilities)
+    @ModifyArgs(method = "getTabCompletions", at = @At(value = "INVOKE", target = "net/minecraft/command/CommandLocate.getListOfStringsMatchingLastWord([Ljava/lang/String;[Ljava/lang/String;)Ljava/util/List;"))
+    private void moreplanets$addStructures(Args args)
     {
-        List<String> newLoc = Lists.newArrayList("CrashedAlienShip", "AbandonedSatellite", "DionaMineshaft", "CheeseSporeHut", "NibiruDungeon", "NibiruVillage", "NibiruStronghold", "NibiruPyramid", "NibiruOceanMonument", "NibiruMineshaft", "NibiruJungleTemple", "NibiruIgloo", "RongHouse");
-        List<String> mod = Arrays.stream(possibilities).collect(Collectors.toList());
-        mod.addAll(newLoc);
-        return CommandBase.getListOfStringsMatchingLastWord(args, mod);
+        String[] original = args.get(1);
+        String[] moreplanetsStructures = new String[] { "CrashedAlienShip", "AbandonedSatellite", "DionaMineshaft", "CheeseSporeHut", "NibiruDungeon", "NibiruVillage", "NibiruStronghold", "NibiruPyramid", "NibiruOceanMonument", "NibiruMineshaft", "NibiruJungleTemple", "NibiruIgloo", "RongHouse" };
+        String[] modified = ArrayUtils.addAll(original, moreplanetsStructures);
+        Arrays.sort(modified);
+        args.set(1, modified);
     }
 }
--- src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
+++ src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
@@ -2,26 +2,25 @@ package stevekung.mods.moreplanets.core.mixin.gc;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 import micdoodle8.mods.galacticraft.core.event.EventHandlerGC;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import stevekung.mods.moreplanets.utils.SurvivalPlanetUtils;
 
 @Mixin(EventHandlerGC.class)
 public class MixinEventHandlerGC
 {
-    @Redirect(method = "onPlayerRightClickedBlock", remap = false, slice = @Slice(from = @At(value = "FIELD", target = "net/minecraft/init/Blocks.TNT:Lnet/minecraft/block/Block;")), at = @At(value = "INVOKE", target = "net/minecraftforge/event/entity/player/PlayerInteractEvent$RightClickBlock.setCanceled(Z)V", remap = false))
-    private void moreplanets$enableIgnitedItem(PlayerInteractEvent.RightClickBlock event, boolean defaultValue)
+    @ModifyArg(method = "onPlayerRightClickedBlock", remap = false, slice = @Slice(from = @At(value = "FIELD", target = "net/minecraft/init/Blocks.TNT:Lnet/minecraft/block/Block;")), at = @At(value = "INVOKE", target = "net/minecraftforge/event/entity/player/PlayerInteractEvent$RightClickBlock.setCanceled(Z)V", ordinal = 0, remap = false))
+    private boolean moreplanets$enableIgnitedItem(boolean original)
     {
         if (SurvivalPlanetUtils.hasSurvivalPlanetData() || SurvivalPlanetUtils.hasSurvivalPlanetDataForServer())
         {
-            event.setCanceled(false);
+            return false;
         }
         else
         {
-            event.setCanceled(defaultValue);
+            return original;
         }
     }
 }
