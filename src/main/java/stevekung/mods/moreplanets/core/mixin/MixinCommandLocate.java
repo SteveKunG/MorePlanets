d7c5d7ad4c4b8a26a89cfb8723dadb1f572a1436
SteveKunG <mccommander_minecraft@hotmail.com>
2024-01-08 16:44:16 +0700

    Use ModifyArg instead

--- src/main/java/stevekung/mods/moreplanets/core/mixin/MixinCommandLocate.java
+++ src/main/java/stevekung/mods/moreplanets/core/mixin/MixinCommandLocate.java
@@ -5,21 +5,19 @@ import java.util.Arrays;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.command.CommandLocate;
 
 @Mixin(CommandLocate.class)
 public class MixinCommandLocate
 {
-    @ModifyArgs(method = "getTabCompletions", at = @At(value = "INVOKE", target = "net/minecraft/command/CommandLocate.getListOfStringsMatchingLastWord([Ljava/lang/String;[Ljava/lang/String;)Ljava/util/List;"))
-    private void moreplanets$addStructures(Args args)
+    @ModifyArg(method = "getTabCompletions", at = @At(value = "INVOKE", target = "net/minecraft/command/CommandLocate.getListOfStringsMatchingLastWord([Ljava/lang/String;[Ljava/lang/String;)Ljava/util/List;"), index = 1)
+    private String[] moreplanets$addStructures(String[] original)
     {
-        String[] original = args.get(1);
         String[] moreplanetsStructures = new String[] { "CrashedAlienShip", "AbandonedSatellite", "DionaMineshaft", "CheeseSporeHut", "NibiruDungeon", "NibiruVillage", "NibiruStronghold", "NibiruPyramid", "NibiruOceanMonument", "NibiruMineshaft", "NibiruJungleTemple", "NibiruIgloo", "RongHouse" };
         String[] modified = ArrayUtils.addAll(original, moreplanetsStructures);
         Arrays.sort(modified);
-        args.set(1, modified);
+        return modified;
     }
 }
