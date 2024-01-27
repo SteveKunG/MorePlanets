7111097f913821ae80b283732706aa02f4bbb3b6
SteveKunG <mccommander_minecraft@hotmail.com>
2024-01-28 00:58:18 +0700

    Fix mixin injection for right clicking on obsidian block

--- src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
+++ src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
@@ -3,7 +3,6 @@ package stevekung.mods.moreplanets.core.mixin.gc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Slice;
 
 import com.llamalad7.mixinextras.sugar.Local;
 
@@ -15,7 +14,7 @@ import stevekung.mods.moreplanets.utils.SurvivalPlanetUtils;
 @Mixin(EventHandlerGC.class)
 public class MixinEventHandlerGC
 {
-    @ModifyArg(method = "onPlayerRightClickedBlock", remap = false, slice = @Slice(from = @At(value = "FIELD", target = "net/minecraft/init/Blocks.TNT:Lnet/minecraft/block/Block;")), at = @At(value = "INVOKE", target = "net/minecraftforge/event/entity/player/PlayerInteractEvent$RightClickBlock.setCanceled(Z)V", ordinal = 0, remap = false))
+    @ModifyArg(method = "onPlayerRightClickedBlock", remap = false, at = @At(value = "INVOKE", target = "net/minecraftforge/event/entity/player/PlayerInteractEvent$RightClickBlock.setCanceled(Z)V", ordinal = 4, remap = false))
     private boolean moreplanets$enableIgnitedItem(boolean original, @Local Block blockClicked)
     {
         if (blockClicked == Blocks.OBSIDIAN && (SurvivalPlanetUtils.hasSurvivalPlanetData() || SurvivalPlanetUtils.hasSurvivalPlanetDataForServer()))
