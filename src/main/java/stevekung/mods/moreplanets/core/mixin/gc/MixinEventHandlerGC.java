5b28d6018821133fec63465f5a4c89ce96aa4abf
SteveKunG <mccommander_minecraft@hotmail.com>
2024-01-28 00:33:15 +0700

    Fix can ignite fire in the space when the player in survival planet mode

--- src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
+++ src/main/java/stevekung/mods/moreplanets/core/mixin/gc/MixinEventHandlerGC.java
@@ -5,16 +5,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
+import com.llamalad7.mixinextras.sugar.Local;
+
 import micdoodle8.mods.galacticraft.core.event.EventHandlerGC;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import stevekung.mods.moreplanets.utils.SurvivalPlanetUtils;
 
 @Mixin(EventHandlerGC.class)
 public class MixinEventHandlerGC
 {
     @ModifyArg(method = "onPlayerRightClickedBlock", remap = false, slice = @Slice(from = @At(value = "FIELD", target = "net/minecraft/init/Blocks.TNT:Lnet/minecraft/block/Block;")), at = @At(value = "INVOKE", target = "net/minecraftforge/event/entity/player/PlayerInteractEvent$RightClickBlock.setCanceled(Z)V", ordinal = 0, remap = false))
-    private boolean moreplanets$enableIgnitedItem(boolean original)
+    private boolean moreplanets$enableIgnitedItem(boolean original, @Local Block blockClicked)
     {
-        if (SurvivalPlanetUtils.hasSurvivalPlanetData() || SurvivalPlanetUtils.hasSurvivalPlanetDataForServer())
+        if (blockClicked == Blocks.OBSIDIAN && (SurvivalPlanetUtils.hasSurvivalPlanetData() || SurvivalPlanetUtils.hasSurvivalPlanetDataForServer()))
         {
             return false;
         }
